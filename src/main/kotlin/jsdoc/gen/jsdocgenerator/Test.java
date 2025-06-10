f3b8c55bdc1c438bb7ab5dc7dd855139ead41bac
jonggurl <jonggurl96@naver.com>
2025-06-10 13:49:45 +0900

    Test.java getter, setter 추가

--- src/main/kotlin/jsdoc/gen/jsdocgenerator/Test.java
+++ src/main/kotlin/jsdoc/gen/jsdocgenerator/Test.java
@@ -22,6 +22,8 @@ public class Test {
 	 */
 	private int age;
 	
+	public Test() {}
+	
 	public Test(String name, int age) {
 		this.name = name;
 		this.age = age;
@@ -29,6 +31,18 @@ public class Test {
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
 	}
 	
 }
